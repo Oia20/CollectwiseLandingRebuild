15cc4791d962c6999dcea56d6ffb216268b5b884
Jacob <92279567+Oia20@users.noreply.github.com>
2025-01-28 13:59:12 -0600

    Changed favicon, added sitemap/robot.txt, added meta tags.

--- collectWiseLanding/astro.config.mjs
+++ collectWiseLanding/astro.config.mjs
@@ -5,7 +5,10 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: 'https://collectwiselandingrebuild.pages.dev',
+  integrations: [react(), tailwind(), sitemap()]
 });
